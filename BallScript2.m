e6cf47ee338481f68a25695e68323d268f66bd41
richarthurs <Rich@Richards-Mac-Pro.local>
2016-03-21 23:43:52 -0700

    Begin working on curve ball implementati
    Mathy math!

---
+++ BallScript2.m
@@ -0,0 +1,12 @@
+thetaMin = pi/2;
+thetaMax = 1.5*pi;
+range = thetaMax - thetaMin;
+
+steps = 10;
+
+for i = 1:steps
+    theta = thetaMin + (range 
+    
+    display(i)
+    
+end
---
+++ BallTime.m
@@ -0,0 +1,7 @@
+function [ output_args ] = BallTime( input_args )
+%BALLTIME Summary of this function goes here
+%   Detailed explanation goes here
+
+
+end
+
