8759c8be11f3366851c08eb767b04aafe8ee79a8
richarthurs <rich@richarthurs.com>
2016-03-24 12:05:00 -0700

    Changed Xyball
    Renamed f to y to make it easier to understand.

--- xyBall.m
+++ xyBall.m
@@ -6,10 +6,10 @@
 r = 0.07;    % radius of track
 g = -9.8;
 syms x;
-f = -sqrt(r^2 - x^2);  % equation of track from r^2 = x^2 + y^2, solved for y
+y = -sqrt(r^2 - x^2);  % equation of track from r^2 = x^2 + y^2, solved for y
 
 %t = (1/sqrt(2*g))* int(sqrt((1 + (diff(f))^2)/(f)),x,0, 0.0699);
-t = (1/sqrt(2*g))* int(sqrt((1 + (diff(f))^2)/(f)),x,0, 0.0699);
+t = (1/sqrt(2*g))* int(sqrt((1 + (diff(y))^2)/(y)),x,0, 0.0699);
 
 disp(vpa(t))
 
---
+++ xyBallNoSlip.m
@@ -0,0 +1,24 @@
+% this function uses x and Y coordinates to determine the amount of time a
+% ball took to fall down a ramp described by equation f
+% https://ca.answers.yahoo.com/question/index?qid=20101229213823AAjuJJS
+% It assumes no slipping!
+
+r = 0.5;    % radius of track
+R = 0.01;   % radius of ball
+g = 9.81;    % gravity
+
+syms x;
+y = r^2 - x^2;  % equation of track
+
+% t = int(sqrt(((10/7)*g*(f))/(1+diff(f)^2)),x,0, 0.3);
+
+
+t = (sqrt((10/7)*g)) * int(sqrt((1 + diff(y)^2)/(y)),x,0, 0.3);
+%                          int(sqrt((1 + diff(f)^2)/(f)),x,0, 0.3);
+disp(vpa(t))
+
+
+
+
+
+
