febbc40abe75002ea0f24cca8d80e7b64eba3476
gabesoares <gsoares026@gmail.com>
2016-03-29 19:56:27 -0700

    Update initSpring.m

--- initSpring.m
+++ initSpring.m
@@ -1,4 +1,4 @@
-function v = initSpring(x,k)
+function [F_spring, data_new] = initSpring(data_old)
 %declare the global variables that are needed throughout 
 global mass;
 global velocity;
@@ -8,15 +8,14 @@ global r;           % Radius of the ball
 
 syms v
 v = vpasolve(v == (k^(1/2)*x)/mass^(1/2),v);
-velocity = [v, 0];
+data_new(1,4) = v; % Updates the data matrix with the initial x velocity. Note that the y velocity remains zero.
+data_new(1,6) = v*r; % Updates the data matrix with the initial angular velocity. (Rolls without slipping)
+
 
 +syms F_spring
  +F_spring = vpasolve(F_spring == (s/impact_time)*(5*k*mass/7)^(1/2), F_spring);
  +
- +% Assuming that the frictional force does not affect the acceleration of the ball, it moves at constant velocity
- +% Without any linear or angular acceleration. 
- +
- +syms omega
- +omega = v*r;
+ +% Assuming that the frictional force does not affect the acceleration of the ball, it moves at constant velocity.
+ +% i.e. The linear and angular acceleration remain zero.
 
 end
