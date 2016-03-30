1a87886128a42294a3440dd38ad51621b2e37ffd
gabesoares <gsoares026@gmail.com>
2016-03-30 16:53:16 -0700

    Update fHammerImpact.m

--- fHammerImpact.m
+++ fHammerImpact.m
@@ -1,10 +1,21 @@
-function [F_spring, data_new] = initSpring(data_old)
+function [data_new] = HammerImpact(data_old)
 % The required global variables are initialized in the fSystemInit function
 
-data_new = data_old;
+global mass_hammer;
+global mass_ball;
+global g;
+g = 9.81; % Global gravity value in fSystemInit is negative...
+
+h_1 = 0.11; % In metres, the diistance the centre of gravity travels from top to point of impact.
+h_2 = 0.05; % In metres, the height the centre of gravity of the hammer travels after impact.
+d = 0.055; % In metres, the distance from the centre of gravity of the hammer to the point of rotation.
+r = 0.01; % In metres, the radius of the cylindrical hammer head.
+
+[p, q] = size(data_old);
+data_new = data_old(p,:);
 
 syms v
-v = vpasolve(v == (k^(1/2)*x)/mass^(1/2),v);
+v = vpasolve(v == (mass_hammer/mass_ball)*(sqrt(2*g*h_2)-d*sqrt((2*g*h_1)/(.5(r^2) + d^2))),v);
 data_new(1,4) = v; % Updates the data matrix with the initial x velocity. Note that the y velocity remains zero.
 data_new(1,6) = v./R; % Updates the data matrix with the initial angular velocity. (Rolls without slipping)
 
