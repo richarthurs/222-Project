4658b59b531690e47c5bcdb9b4f2cec0c722d497
gabesoares <gsoares026@gmail.com>
2016-03-29 18:45:18 -0700

    Update fSystemInit.m

--- fSystemInit.m
+++ fSystemInit.m
@@ -2,17 +2,19 @@ function [  ] = SystemInit(  )
 % This function initializes the global variables required for the
 % simulation. Change ball parameters here. 
 
-global g;
+global g;   % Gravity
 global R;   % Radius of Ball
 global m;   % Mass of Ball
 global I;   % Moment of inertia of ball
 global steps;
+global k;   % Spring constant of the ball
 
 g = -9.81;
 R = 0.01;
 m = 0.03;
 I = 0.4 * m * R^2;
 steps = 10;
+k = 100;
 
 end
 
