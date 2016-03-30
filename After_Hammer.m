5262713f7dff5b892479b8559ab5cac522e2c091
HarrisonHandley <Harrisonlhandley@gmail.com>
2016-03-29 20:41:19 -0700

    Added files via upload

---
+++ After_Hammer.m
@@ -0,0 +1,34 @@
+function [ Master_Array ] = After_Hammer( Master_Array, Max_Theta, Circle_radius )
+%UNTITLED Summary of this function goes here
+%   Detailed explanation goes here
+
+%Variables
+%t
+%Vx
+%Vy
+%Start_theta
+%Start_AngVel
+%Cur_theta = Start_theta
+%Cur_AngVel = From Array
+%CurPx
+%CurPy
+%StartPx
+%StartPy
+
+%Entered data from measurements
+%Max_Theta
+
+global steps;   % the number of steps to compute in iterative functions
+global I;   % moment of inertia of ball
+global m;   % mass of ball
+global g;   % acceleration due to gravity
+global R;   % Radius of Ball
+
+%Need to relate t to theta in some fashion
+while(Cur_theta <= Max_Theta)
+    Cur_AngVel=
+end
+
+    
+end
+
