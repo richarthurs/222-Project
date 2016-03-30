464c358462db08d5f3372218f8b3ca11fa771f25
gabesoares <gsoares026@gmail.com>
2016-03-29 19:04:07 -0700

    Update initSpring.m

--- initSpring.m
+++ initSpring.m
@@ -5,7 +5,9 @@ global velocity;
 global impact_time; % Impact time between the spring and the ball
 global s; % Distance compressed by the spring
 global r; % Radius of the ball
+global k; % Spring constant of the ball
 
+impact_time = 0.00141; % Time in seconds
 
 syms v
 v = vpasolve(v == ((5k^(1/2))*x)/(7*mass^(1/2)),v);
