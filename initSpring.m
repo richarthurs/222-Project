96c0faed950d793f406ee3b73008d53b3f6dbc85
gabesoares <gsoares026@gmail.com>
2016-03-30 09:45:15 -0700

    Update initSpring.m

--- initSpring.m
+++ initSpring.m
@@ -17,10 +17,10 @@ F_normal = m*g;
 
 data_new(1,10) = F_normal; % Adds the initial normal force into the normal force column of the master data matrix.
 
-+syms F_spring
- +F_spring = vpasolve(F_spring == (s/impact_time)*(5*k*mass/7)^(1/2), F_spring);
- +
- +% Assuming that the frictional force does not affect the acceleration of the ball, it moves at constant velocity.
- +% i.e. The linear and angular acceleration remain zero.
+syms F_spring
+F_spring = vpasolve(F_spring == (s/impact_time)*(5*k*mass/7)^(1/2), F_spring);
+ 
+% Assuming that the frictional force does not affect the acceleration of the ball, it moves at constant velocity.
+% i.e. The linear and angular acceleration remain zero.
 
 end
