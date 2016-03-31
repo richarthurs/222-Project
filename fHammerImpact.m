b4a9222b058efbdb16716b6d8641f34c050f92a2
gabesoares <gsoares026@gmail.com>
2016-03-31 11:49:59 -0700

    Updated Velocity & Acceleration of Hammer

--- fHammerImpact.m
+++ fHammerImpact.m
@@ -1,7 +1,7 @@
 function [F_hammer, data_new] = HammerImpact(data_old)
 % The required global variables are initialized in the fSystemInit function
 
-R = 0.01; %0.01 metres
+R = 0.01; % 0.01 metres
 g = 9.81; % Global gravity value in fSystemInit is negative...
 mass_hammer = 0.2; %200 grams
 mass_ball = 0.03; %30 grams
@@ -31,8 +31,24 @@ F_normal = mass_ball*g; % No acceleration in the y-direction, so the normal forc
 
 data_new(1,10) = F_normal; % Adds the initial normal force into the normal force column of the master data matrix.
 
+% Time taken for the hammer to fall from top to bottom is approximately 0.5 seconds
+% Time taken from hammer to go from bottom position to top after hitting the ball is approximately 0.34 seconds
+
 syms F_hammer
 F_hammer = vpasolve(F_hammer == (mass_ball*v_ball/0.00141), F_hammer); % 0.00141 is the estimated impact time in seconds.
+
+wi_hammer = sqrt((2*g*h_1)/(.5r^2+d^2)); % Angular velocity of the hammer right before impact with the ball
+w_int_hammer = sqrt(2*g*h_2); % Intermediate angular velocity, right after impact with the ball
+wf_hammer = 0; % Angular velocity of the hammer as it rises and comes to rest before falling down again.
+
+% Using the kinematic formulas and assuming constant acceleration, the linear and angular accelerations can be found
+% The time intervals have been measured experimentally
+
+% Assuming constant linear and angular acceleration
+alpha_1_hammer = (wi_hammer)/0.5; % Angular acceleration of hammer from top to bottom
+alpha_2_hammer = (wf_hammer - w_int_hammer)/0.34; % Angular acceleration of hammer from after impact to top as it follows through
+
+a_1
 
 data_new = [data_old; data_new];
  
