1520606e0a9da35d700e9ed66c40871ba99ba1ff
gabesoares <gsoares026@gmail.com>
2016-04-01 21:36:26 -0700

    Included Free Fall

--- fProjectileMotion.m
+++ fProjectileMotion.m
@@ -3,7 +3,7 @@ function [data_new] = ProjectileMotion(data_old)
 
 global R; % Radius of ball
 global t_inc;
-t_inc = 0.01;
+t_inc = 0.001;
 R = 0.01;
 
 [p, q] = size(data_old);
@@ -16,41 +16,41 @@ vy_i = data_old(p,5); % Get the initial y velocity from the master array
 data_current = data_old(p,:);
 data_new = data_old;
 
-for t=t_inc:t_inc:1  % If we start at 0 instead of t_inc, then we'll duplicate the last layer of the master matrix
- 
-    
- if x < (0.05 - R)
+%% Constant parameters during the projectile motion
+
+data_current(1,6) = 0; % Angular velocity is assumed to be zero
+data_current(1,7) = 0; % x component of acceleration remains zero
+data_current(1,8) = -9.81; % y component of acceleration remains g
+data_current(1,9) = 0; % Angular acceleration is assumed to be zero
+
+%% Changing position and velocity
+x = 0; % Current x position relative to starting position
+y = 0; % Current y position relative to starting position
+
+for t=t_inc:t_inc:2  % If we start at 0 instead of t_inc, then we'll duplicate the last layer of the master matrix
+
+ if x < (0.05 - R) && y > -0.0633
  % We want to keep incrementing the x values.
     x = vx_i*t; % Current x position relative to starting position 
- elseif x >= (0.05 - R)
-     x = 0;
- elseif y == -0.0633 % When the ball has fallen enough that it has reached the curved landing
+ elseif x >= (0.05 - R) && y > -0.0633 % If the ball has hit the ball but still has space to fall downward:
+     % Do nothing
+ elseif x >= (0.05 - R) && y <= -0.0633
+     break
+ elseif x < (0.05 - R) && y <= -0.0633
      break
  end
  
-
-     
-    
-
  y = t*vy_i - .5*(9.81)*t^2; % Current y position relative to starting position
  
  v_y = vy_i - (9.81)*t; % Current y velocity
- 
+  
  data_current(1,1) = data_current(1,1) + t_inc; % Increment the time by t_inc
  data_current(1,2) = x_i + x; % Update master matrix with absolute x position of the ball
  data_current(1,3) = y_i + y; % Update master matrix with absolute y position of the ball
  data_current(1,5) = v_y; % Update the y velocity of the ball, x velocity remains constant
 
  data_new = [data_new; data_current];
- 
- if x >= (0.05 - R)
-  % If this if-statement executes, we know the ball has hit the wall.
-  % Then, we will consider the ball falling straight vertically downwards,
-  % x position remains constant.
-  x = 0;
-  break
- end
- 
+
 end
 
  figure
