d0ededc08821a339ead431fcbd5cb0ff27b7fa4e
gabesoares <gsoares026@gmail.com>
2016-04-03 16:31:03 -0700

    Fixed Velocity of Ball and Angular Velocity of Hammer
    Forgot to output the Hammer data, gotta output an array of angular velocities and accelerations

--- fHammerImpact.m
+++ fHammerImpact.m
@@ -6,8 +6,7 @@ g = 9.81; % Global gravity value in fSystemInit is negative...
 mass_hammer = 0.092; % 92 grams
 mass_ball = 0.016; % 16 grams
 
-h_1 = 0.11; % In metres, the diistance the centre of gravity travels from top to point of impact.
-h_2 = 0.05; % In metres, the height the centre of gravity of the hammer travels after impact.
+h = 0.05; % In metres, the height the centre of gravity of the hammer travels after impact.
 d = 0.055; % In metres, the distance from the centre of gravity of the hammer to the point of rotation.
 r = 0.01; % In metres, the radius of the cylindrical hammer head.
 
@@ -21,7 +20,7 @@ r = 0.01; % In metres, the radius of the cylindrical hammer head.
 data_new = data_old(p,:);
 
 syms v_ball
-v_ball = vpasolve(v_ball == (mass_hammer/mass_ball)*(d*sqrt((2*g*h_1)/(.5*r^2 + d^2))-sqrt(2*g*h_2)),v_ball);
+v_ball = vpasolve(v_ball == (mass_hammer/mass_ball)*d*sqrt(2*9.81/(.5*r^2+d^2))*(sqrt(2*d)-sqrt(h)));
 data_new(1,4) = v_ball; % Updates the data matrix with the x velocity. Note that the y velocity remains zero.
 data_new(1,6) = v_ball./R; % Updates the data matrix with the angular velocity. (Rolls without slipping)
 
@@ -37,8 +36,8 @@ data_new(1,10) = F_normal; % Adds the initial normal force into the normal force
 syms F_hammer
 F_hammer = vpasolve(F_hammer == (mass_ball*v_ball/0.00141), F_hammer); % 0.00141 is the estimated impact time in seconds.
 
-wi_hammer = sqrt((2*g*h_1)/(.5*r^2+d^2)); % Angular velocity of the hammer right before impact with the ball
-w_int_hammer = sqrt(2*g*h_2); % Intermediate angular velocity, right after impact with the ball
+wi_hammer = sqrt((4*g*d)/(.5*r^2+d^2)); % Angular velocity of the hammer right before impact with the ball
+w_int_hammer = sqrt((2*g*h)/(0.5*r^2+d^2)); % Intermediate angular velocity, right after impact with the ball
 wf_hammer = 0; % Angular velocity of the hammer as it rises and comes to rest before falling down again.
 
 % Using the kinematic formulas and assuming constant acceleration, the linear and angular accelerations can be found
