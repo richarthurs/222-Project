71b09799b82d463fdc43a7b5dee3a28ae4383372
gabesoares <gsoares026@gmail.com>
2016-04-03 16:56:21 -0700

    Output for Hammer Data over the entire swing

--- fHammerImpact.m
+++ fHammerImpact.m
@@ -1,5 +1,7 @@
-function [F_hammer, data_new] = HammerImpact(data_old)
+function [Hammer_Data, F_hammer, data_new] = HammerImpact(data_old)
 % The required global variables are initialized in the fSystemInit function
+
+global t_inc;
 
 R = 0.01; % 0.01 metres
 g = 9.81; % Global gravity value in fSystemInit is negative...
@@ -36,8 +38,8 @@ data_new(1,10) = F_normal; % Adds the initial normal force into the normal force
 syms F_hammer
 F_hammer = vpasolve(F_hammer == (mass_ball*v_ball/0.00141), F_hammer); % 0.00141 is the estimated impact time in seconds.
 
-wi_hammer = sqrt((4*g*d)/(.5*r^2+d^2)); % Angular velocity of the hammer right before impact with the ball
-w_int_hammer = sqrt((2*g*h)/(0.5*r^2+d^2)); % Intermediate angular velocity, right after impact with the ball
+wi_hammer = sqrt((4*9.81*d)/(.5*r^2+d^2)); % Angular velocity of the hammer right before impact with the ball
+w_int_hammer = sqrt((2*9.81*h)/(0.5*r^2+d^2)); % Intermediate angular velocity, right after impact with the ball
 wf_hammer = 0; % Angular velocity of the hammer as it rises and comes to rest before falling down again.
 
 % Using the kinematic formulas and assuming constant acceleration, the linear and angular accelerations can be found
@@ -51,6 +53,37 @@ a_1 = alpha_1_hammer*d;
 a_2 = alpha_2_hammer*d;
 
 data_new = [data_old; data_new];
+
+Hammer_Data = zeros(1,5); % time, velocity, angular velocity, acceleration, angular acceleration
+
+%% First part of the hammer swing, until impact
+
+for t = t_inc:t_inc:0.5
+
+ Current_Data = zeros(1,5);
+ Current_Data(1,1) = t + data_old(p,1); % The current time we're at starting from when the ball hits the bottom of the hammer.
+ Current_Data(1,2) = alpha_1_hammer*t*d; % Current velocity of the hammer centre of gravity
+ Current_Data(1,3) = alpha_1_hammer*t; % Current angular velocity of the hammer
+ Current_Data(1,4) = a_1; % Constant linear acceleration of the hammer centre of gravity
+ Current_Data(1,5) = alpha_1_hammer; % Constant angular acceleration of the hammer
+ 
+ Hammer_Data = [Hammer_Data; Current_Data];
+end
+
+%% Second part of the hammer swing, after impact till it reaches maximum height
+
+for i = t_inc:t_inc:0.34
+
+ Current = zeros(1,5);
+ Current(1,1) = i + 0.5 + data_old(p,1); % Current time
+ Current(1,2) = alpha_2_hammer*i*d; % Current velocity of the hammer centre of gravity
+ Current(1,3) = w_int_hammer - alpha_2_hammer*t; % Current angular velocity of the hammer
+ Current(1,4) = a_2; % Constant linear acceleration of the hammer centre of gravity
+ Current(1,5) = alpha_2_hammer; % Constant angular acceleration of the hammer
+ 
+ Hammer_Data = [Hammer_Data; Current];
+end
+
  
 % Assuming that the frictional force does not affect the acceleration of the ball, it moves at constant velocity.
 % i.e. The linear and angular acceleration remain zero.
